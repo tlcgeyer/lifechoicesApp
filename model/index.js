634f3eb428be73aaccb8e688f5f25de57ad296ef
tlcgeyer <tamlincupido2402@gmail.com>
2024-02-15 15:11:43 +0200

    backend

---
+++ model/index.js
@@ -0,0 +1,11 @@
+import {Users} from "./Users.js"
+import { Products } from "./Products.js";
+
+// Creating an object
+let users = new Users ()
+let products = new Products ()
+
+export {
+    users,
+    products
+}
