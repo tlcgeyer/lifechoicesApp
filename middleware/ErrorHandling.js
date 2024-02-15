49c4e6aebf9795e96a97c8efec9c7b73101dbfde
tlcgeyer <tamlincupido2402@gmail.com>
2024-02-15 11:37:26 +0200

    middleware

---
+++ middleware/ErrorHandling.js
@@ -0,0 +1,16 @@
+// Creating a error handling middleware
+/*If we passing next as an argument it means that we are making use of a middleware */
+function errorHandling(err, req, res, next ) {
+    if(err || res.statusCode >= 400) {
+        res.json({
+            statusCode: err.status || res.
+            statusCode || 500, //problem with the api/server
+            msg: 'Apologies, there seems to be an error in the server. Please try again later.'
+        })
+    }else {
+        next()
+    }
+}
+export {
+    errorHandling
+}
