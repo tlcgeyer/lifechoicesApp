96bffdbcf42323d90473f59db81a7892b1b558cb
tlcgeyer <tamlincupido2402@gmail.com>
2024-02-15 09:19:38 +0200

    config

--- config/index.js
+++ config/index.js
@@ -1,3 +1,17 @@
-import express from 'express'
-import { createPool } from 'mysql'
-import bodyParser from 'body-parser'
+import { createPool } from 'mysql';
+import {config} from "dotenv"
+config() //Importing mysql data for we will be working with a database.
+
+//Creating a database connection: we created a pool for us to set up the database connection
+// The port number is set by default hence there is no need to add one. 
+let connection = createPool({
+    host: process.env.HOST,
+    database: process.env.DBName,
+    user: process.env.UserName,
+    password: process.env.UserPass,
+    multipleStatements: true ,
+    connectionLimit: 30
+})
+export {
+    connection
+}
