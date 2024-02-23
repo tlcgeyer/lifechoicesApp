83ac203c8bdf5b89b6e6a9b6e0e25e9aabb5417d
Tamlin Geyer <146750294+tlcgeyer@users.noreply.github.com>
2024-02-23 16:21:00 +0200

    Update ProductsView.vue

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,12 +1,28 @@
 <template>
-    <div>
-
+    <div class="container">
+        <div class="row">
+            <div class="row" v-if="products">
+                <Card v-for="product in products" :key="product.prodID">
+                    <template #cardHeader>
+                        <h4>{{product.prodName}}</h4>
+            </div>        
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'ProductsView'
+      components: {
+          CardComp
+      },
+        computed: {
+            products() {
+                return this.$store.state.products
+            }
+        },
+        mounted() {
+            this.$store.dispatch('fetchProducts')
+        }
     }
 </script>
 
