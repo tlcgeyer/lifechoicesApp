964f286a1a59a727a063be04150d40aacd98b8a3
tlcgeyer <tamlincupido2402@gmail.com>
2024-02-23 11:13:53 +0200

    storeinfo

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore } from 'vuex'
-import axios from 'axios;'
+import axios from 'axios'
 import sweet from 'sweetalert'
-import {useCookies}  'vue3-cookies'
+import {useCookies} from 'vue3-cookies'
 const {cookies} = useCookies()
 const lifeURL = 'https://lifechoicesapp-kril.onrender.com/'
 
@@ -9,30 +9,32 @@ const lifeURL = 'https://lifechoicesapp-kril.onrender.com/'
 
 export default createStore({
   state: {
-    users:null,
-    user:null,
-    product:null,
-    products:null
+    // users:null,
+    // user:null,
+    // product:null,
+    // products:null
   },
   getters: {
   },
   mutations: {
-    setUsers(state,value) {
-      state.users= value
-    },
-    setUser(state,value) {
-      state.user= value
-    },
-    setProduct(state,value) {
-      state.product= value
-    },
-    setProducts(state,value) {
-      state.products= value
-    },
-
+    // setUsers(state,value) {
+    //   state.users= value
+    // },
+    // setUser(state,value) {
+    //   state.user= value
+    // },
+    // setProduct(state,value) {
+    //   state.product= value
+    // },
+    // setProducts(state,value) {
+    //   state.products= value
+    // }
 
   },
   actions: {
+    // async register(context, payload) {
+    //   let {msg} = await axios.post(``)
+    // }
   },
   modules: {
   }
